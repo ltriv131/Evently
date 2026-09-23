782e1239cbd42ca9ccebaffc5f3a1ebb6c86cbff
Liam <ltrivers131@gmail.com>
2026-09-22 22:35:33 -0400

    docker containers and more hydration error fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <Navbar />
         <main className={styles.main}>{children}</main>
